d27a91c2a04afb7435cdde715ded05234a13847b
Josh-presentTense <200464522@student.georgianc.on.ca>
2021-02-10 01:41:22 -0500

    Override the toString method in the Card class

--- src/Card.java
+++ src/Card.java
@@ -41,4 +41,8 @@ public class Card {
     public void setFaceValue(int faceValue) {
         this.faceValue = faceValue;
     }
+
+    public String toString(){
+        return faceName + " of " + suit;
+    }
 }
--- src/CardTester.java
+++ src/CardTester.java
@@ -1,2 +1,6 @@
 public class CardTester {
+    public static void main(String[] args) {
+        Card aceOfSpades = new Card("Ace", "Spades", 14);
+        System.out.println(aceOfSpades);
+    }
 }
