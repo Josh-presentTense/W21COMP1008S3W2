6015273f38b214c6c0c0319535328eecb48a3273
Josh-presentTense <200464522@student.georgianc.on.ca>
2021-02-10 17:39:59 -0500

    week 4 work, added Purchases.java

--- W21COMP1008S3W4/src/Main.java
+++ W21COMP1008S3W4/src/Main.java
@@ -2,5 +2,15 @@ public class Main {
     public static void main(String[] args) {
         GroceryItem item1 = new GroceryItem("carrots", 4.79, "vegetables");
         GroceryItem item2 = new GroceryItem("bread", 2.79, "bread");
+
+        Purchases shoppingCart = new Purchases();
+        shoppingCart.addGroceryItem(item1);
+        shoppingCart.addGroceryItem(item2);
+        double total = shoppingCart.getTotalPrice();
+        System.out.printf("the total price is: $%.2f", total);
+
+        GroceryItem item3 = new GroceryItem("ice cream", 7.89, "dairy");
+        shoppingCart.addGroceryItem(item3);
+        System.out.println(shoppingCart);
     }
 }
